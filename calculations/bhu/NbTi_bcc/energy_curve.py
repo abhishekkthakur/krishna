5b21f9f792e385b5e35d21e7875497587f839c68
thakurab <abhishekkt.met16@itbhu.ac.in>
2020-02-08 16:23:54 -0700

    08.feb.2020

---
+++ calculations/bhu/NbTi_bcc/energy_curve.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+xvalues = []
+yvalues = []
+f = open('mypredstr.file', 'r')
+data = f.readlines()
+for i in data:
+	a = i.strip('\n')
+	b = a.split()
+	composition = float(b[0])
+	energy = float(b[2])
+	xvalues.append(composition)
+	yvalues.append(energy)
+f.close()
+plt.scatter(xvalues, yvalues, color = 'red', marker = '+', label = 'Predicted structure')
+
+
+xvalues = []
+yvalues = []
+f = open('myfit.file', 'r')
+data = f.readlines()
+for i in data:
+	a = i.strip('\n')
+	b = a.split()
+	composition = float(b[0])
+	energy = float(b[2])
+	xvalues.append(composition)
+	yvalues.append(energy)
+f.close()
+plt.scatter(xvalues, yvalues, color = 'green', marker = 'x', label = 'Known structure')
+
+xvalues = []
+yvalues = []
+f = open('mygs.file', 'r')
+data = f.readlines()
+for i in data:
+	a = i.strip('\n')
+	b = a.split()
+	composition = float(b[0])
+	energy = float(b[2])
+	xvalues.append(composition)
+	yvalues.append(energy)
+f.close()
+plt.plot(xvalues, yvalues, ls = '--')
+plt.scatter(xvalues, yvalues, color = 'blue', marker = '*', label = 'Known ground state')
+#plt.plot(xvalues, yvalues, ls = '--')
+
+
+
+
+plt.xlabel('Composition')
+plt.ylabel('Formation energy')
+plt.xlim([0, 1])
+plt.ylim([-0.005, 0.1])
+plt.legend()
+plt.show()
